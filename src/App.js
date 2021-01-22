9f55a8b74427257dff287896cddd182bfecb0467
ang <angiezafra07@gmail.com>
2021-01-22 15:12:28 -0500

    adding some mobile responsive

--- src/App.js
+++ src/App.js
@@ -5,16 +5,20 @@ import stars from './media/stars.mp4';
 function App() {
   return (
     <>
-      
+      <div className="wrapper">
       <PlanetNav />
+        
+     </div>
+
+      
 
 
-       {/* <video
+        {/* <video
         id="stars" autoPlay loop muted>
         <source
         src={stars}
         type="video/mp4" />
-       </video>  */}
+       </video>   */}
       
 
     </>
